0f35f4fff45fdbb7232ff28536893b153eeafe9a
xRadne <Alex.radne@gmail.com>
2018-01-01 20:26:00 +0100

    Assigned multiple keys to actions

--- public/js/input.js
+++ public/js/input.js
@@ -3,24 +3,30 @@ import Keyboard from './KeyboardState.js';
 export function setupKeyboard(mario) {
     const input = new Keyboard();
 
-    input.addMapping('KeyP', keyState => {
-        if (keyState) {
-            mario.jump.start();
-        } else {
-            mario.jump.cancel();
-        }
-    });
+    ['KeyP', 'Space'].forEach(eventName => {
+        input.addMapping(eventName, keyState => {
+            if (keyState) {
+                mario.jump.start();
+            } else {
+                mario.jump.cancel();
+            }
+        });
+    })    
 
     input.addMapping('KeyO', keyState => {
         mario.turbo(keyState);
     });
 
-    input.addMapping('KeyD', keyState => {
-        mario.go.dir += keyState ? 1 : -1;
+    ['KeyD', 'ArrowRight'].forEach(eventName => {
+        input.addMapping(eventName, keyState => {
+            mario.go.dir += keyState ? 1 : -1;
+        });
     });
 
-    input.addMapping('KeyA', keyState => {
-        mario.go.dir += keyState ? -1 : 1;
+    ['KeyA', 'ArrowLeft'].forEach(eventName => {
+        input.addMapping(eventName, keyState => {
+            mario.go.dir += keyState ? -1 : 1;
+        });
     });
 
     return input;
